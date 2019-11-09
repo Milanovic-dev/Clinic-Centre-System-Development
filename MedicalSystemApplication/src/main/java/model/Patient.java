1310824452fa77b1eb2e66a1b4786ece0765ffa0
Nikola <Nikola@DESKTOP-D61HDQ7>
2019-11-09 14:23:42 +0100

    Pacijent klasa sa dodatnim base klasama

--- MedicalSystemApplication/src/main/java/model/Patient.java
+++ MedicalSystemApplication/src/main/java/model/Patient.java
@@ -1,5 +1,58 @@
 package model;
 
-public class Patient {
+import java.util.ArrayList;
 
+public class Patient extends User{
+
+	private String insuranceId;
+	private MedicalRecord medicalRecord;
+	private ArrayList<Appointment> scheduledAppointments;
+	private ArrayList<Surgery> scheduledSurgeries;
+
+	public Patient()
+	{
+		super();
+	}
+	
+	public Patient(String username, String password, String email, String name, String surname, String city,
+			String address, String state, String phone) {
+		super(username, password, email, name, surname, city, address, state, phone, UserRole.Patient);
+		medicalRecord = new MedicalRecord();
+		scheduledAppointments = new ArrayList<Appointment>();
+		scheduledSurgeries = new ArrayList<Surgery>();
+	}
+
+	public String getInsuranceId() {
+		return insuranceId;
+	}
+
+	public void setInsuranceId(String insuranceId) {
+		this.insuranceId = insuranceId;
+	}
+
+	public MedicalRecord getMedicalRecord() {
+		return medicalRecord;
+	}
+
+	public void setMedicalRecord(MedicalRecord medicalRecord) {
+		this.medicalRecord = medicalRecord;
+	}
+
+	public ArrayList<Appointment> getScheduledAppointments() {
+		return scheduledAppointments;
+	}
+
+	public void setScheduledAppointments(ArrayList<Appointment> scheduledAppointments) {
+		this.scheduledAppointments = scheduledAppointments;
+	}
+
+	public ArrayList<Surgery> getScheduledSurgeries() {
+		return scheduledSurgeries;
+	}
+
+	public void setScheduledSurgeries(ArrayList<Surgery> scheduledSurgeries) {
+		this.scheduledSurgeries = scheduledSurgeries;
+	}
+		
+	
 }
