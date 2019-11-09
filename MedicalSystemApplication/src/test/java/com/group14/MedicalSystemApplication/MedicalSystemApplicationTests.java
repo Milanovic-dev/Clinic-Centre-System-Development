f5492fc15d5171227e500050c9a6fb8f2c9f7486
Nikola <Nikola@DESKTOP-D61HDQ7>
2019-11-09 11:59:43 +0100

    Prvi HEAD commit

---
+++ MedicalSystemApplication/src/test/java/com/group14/MedicalSystemApplication/MedicalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group14.MedicalSystemApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
