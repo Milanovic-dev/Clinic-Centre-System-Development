89b30e41b03ec0dbc7e68b44deb12ead174fbd16
Nikola <Nikola@DESKTOP-D61HDQ7>
2019-11-10 19:18:21 +0100

    Konflikt fix 2

--- MedicalSystemApplication/src/main/java/model/MedicalRecord.java
+++ MedicalSystemApplication/src/main/java/model/MedicalRecord.java
@@ -6,4 +6,19 @@ import java.util.Collection;
 public class MedicalRecord {
 
 	private Collection<PatientMedicalReport> reports;
+	
+	public MedicalRecord()
+	{
+		super();
+	}
+
+	public Collection<PatientMedicalReport> getReports() {
+		return reports;
+	}
+
+	public void setReports(Collection<PatientMedicalReport> reports) {
+		this.reports = reports;
+	}
+	
+	
 }
