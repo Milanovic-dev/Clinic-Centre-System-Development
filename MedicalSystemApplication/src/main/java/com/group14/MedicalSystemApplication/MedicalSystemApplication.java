5d3fb51055f665a8339b714aa3f5b735c8fd606e
Nikola <Nikola@DESKTOP-D61HDQ7>
2019-11-12 18:24:22 +0100

    Patient service i repository

--- MedicalSystemApplication/src/main/java/com/group14/MedicalSystemApplication/MedicalSystemApplication.java
+++ MedicalSystemApplication/src/main/java/com/group14/MedicalSystemApplication/MedicalSystemApplication.java
@@ -3,11 +3,14 @@ package com.group14.MedicalSystemApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import model.Patient;
+import repository.PatientRepository;
+
 @SpringBootApplication
 public class MedicalSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MedicalSystemApplication.class, args);
+		SpringApplication.run(MedicalSystemApplication.class, args);	
 	}
 
 }
